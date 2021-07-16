8997d7d0bd4872e8d91fea989dcf1c072c698f9f
Jason Humphrey <sjasonhumphrey@yahoo.com>
2021-07-16 15:13:55 -0500

    first commit

---
+++ src/component/ProgressBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProgressBar = props => {
+    return (
+        <div className="progress mb-3" style={{ height: '10px' }}>
+            <div className="progress-bar progress-bar-striped progress-bar-animated bg-warning" style={{ width: props.complete + '%' }}></div>
+        </div>
+    );
+};
+
+export default ProgressBar;
